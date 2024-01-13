b6b2d15931a68afd6ea285e05e69c381cda70bd5
Firad <firad.aslanov@edu.rtu.lv>
2024-01-13 10:54:37 -0800

    Day 21: Add description on the new columns

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ st.set_page_config(layout="wide")
 
 col1,  col2 = st.columns(2)
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.png",width=500)
 
 with col2:
     st.title("Firad Aslanov")
@@ -12,3 +12,8 @@ with col2:
     Hi I am Ardit! I am a python programmer. I have experience in Data and Web field. I am studying in RTU.
     """
     st.info(content)
+
+contact = """
+Below you can find some projects and apps. Feel free to contact me. I used Python and Tableu. I wanted to show my skills.
+ """
+st.write(contact)
